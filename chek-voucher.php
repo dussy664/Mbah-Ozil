c27f78bdaa29e2629ba1df08646f2e23f5e1c85e
Mbah Ozil <60285156+BangOzil@users.noreply.github.com>
2020-04-10 20:13:39 +0700

    Update chek-voucher.php

--- chek-voucher.php
+++ chek-voucher.php
@@ -424,3 +424,723 @@ $detail_voucher = curl('https://api.gojekapi.com/gopoints/v3/wallet/vouchers?lim
 ";
         echo "
 ";
+        echo 'Voucher : ' . $nama_voucher8 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher8 . '';
+        echo "
+";
+    } else {
+    }
+    if ($total_voucher == 10) {
+        echo 'Voucher : ' . $nama_voucher . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher1 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher1 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher2 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher2 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher3 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher3 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher4 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher4 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher5 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher5 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher6 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher6 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher7 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher7 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher8 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher8 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher9 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher9 . '
+';
+        echo "
+";
+    } else {
+    }
+    if ($total_voucher == 11) {
+        echo 'Voucher : ' . $nama_voucher . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher1 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher1 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher2 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher2 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher3 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher3 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher4 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher4 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher5 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher5 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher6 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher6 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher7 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher7 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher8 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher8 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher9 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher9 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher10 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher10 . '
+';
+        echo "
+";
+    } else {
+    }
+    if ($total_voucher == 12) {
+        echo 'Voucher : ' . $nama_voucher . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher1 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher1 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher2 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher2 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher3 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher3 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher4 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher4 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher5 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher5 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher6 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher6 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher7 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher7 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher8 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher8 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher9 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher9 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher10 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher10 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher11 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher11 . '
+';
+        echo "
+";
+    } else {
+    }
+    if ($total_voucher == 13) {
+        echo 'Voucher : ' . $nama_voucher . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher1 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher1 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher2 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher2 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher3 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher3 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher4 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher4 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher5 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher5 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher6 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher6 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher7 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher7 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher8 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher8 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher9 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher9 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher10 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher10 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher11 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher11 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher12 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher12 . '
+';
+        echo "
+";
+    } else {
+    }
+    if ($total_voucher == 14) {
+        echo 'Voucher : ' . $nama_voucher . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher1 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher1 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher2 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher2 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher3 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher3 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher4 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher4 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher5 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher5 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher6 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher6 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher7 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher7 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher8 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher8 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher9 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher9 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher10 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher10 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher11 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher11 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher12 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher12 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher13 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher13 . '
+';
+        echo "
+";
+    } else {
+    }
+    if ($total_voucher == 15) {
+        echo 'Voucher : ' . $nama_voucher . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher1 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher1 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher2 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher2 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher3 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher3 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher4 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher4 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher5 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher5 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher6 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher6 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher7 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher7 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher8 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher8 . '';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher9 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher9 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher10 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher10 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher11 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher11 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher12 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher12 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher13 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher13 . '
+';
+        echo "
+";
+        echo "
+";
+        echo 'Voucher : ' . $nama_voucher14 . '';
+        echo "
+";
+        echo 'Kadaluarsa : ' . $batas_voucher14 . '
+';
+        echo "
+";
+  }
+  }
+ echo "\n".color("nevy","Mau Cek Paylater?: y/n ");
+        $pilihan = trim(fgets(STDIN));
+        if($pilihan == "y" || $pilihan == "Y"){
+        echo color("red","===========(CHECK PAYLATER)===========")."\n";
+        sleep (5);
+ $paylater = curl('https://api.gojekapi.com/paylater/v1/user/profile', null, $header);
+ $duit = json_decode($paylater[0]);
+ $status = $duit->data->credit_nonfraud_eligible;
+ $money = $duit->data->credit_limit;
+    echo "\n".color("yellow","Status = ".$status." \n");
+   echo color("yellow","Limit Tersedia = Rp".$money." \n");
+ $transaksi = curl('https://api.gojekapi.com/paylater/v1/user/transactions', null, $header);
+ $data = json_decode($transaksi[0]);
+  if ($data->success == true) {
+ $status1 = $data->transactions[0]->amount;
+ $status2 = $data->transactions[0]->description;
+    echo "\n".color("red","HUTANG = ".$status1." \n");
+   echo color("red","TRANSAKSI DI = ".$status2." \n");
+}else if($pilihan == "n" || $pilihan == "N"){
+         die();
+         }else{
+echo "".color("red","AKUN BELUM UPGRADE \n");
+}
+}
+//CHECKER AKHIR
+ function nama()
+	{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "http://ninjaname.horseridersupply.com/indonesian_name.php");
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$ex = curl_exec($ch);
+	// $rand = json_decode($rnd_get, true);
+	preg_match_all('~(&bull; (.*?)<br/>&bull; )~', $ex, $name);
+	return $name[2][mt_rand(0, 14) ];
+	}
+
+function curl($url, $fields = null, $headers = null)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        if ($fields !== null) {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        }
+        if ($headers !== null) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
+        $result   = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        
+        return array(
+            $result,
+            $httpcode
+        );
+	}
+function color($color = "default" , $text)
+    {
+        $arrayColor = array(
+            'grey'      => '1;30',
+            'red'       => '1;31',
+            'green'     => '1;32',
+            'yellow'    => '1;33',
+            'blue'      => '1;34',
+            'purple'    => '1;35',
+            'nevy'      => '1;36',
+            'white'     => '1;0',
+        );  
+        return "\033[".$arrayColor[$color]."m".$text."\033[0m";
+    }
+function fetch_value($str,$find_start,$find_end) {
+	$start = @strpos($str,$find_start);
+	if ($start === false) {
+		return "";
+	}
+	$length = strlen($find_start);
+	$end    = strpos(substr($str,$start +$length),$find_end);
+	return trim(substr($str,$start +$length,$end));
+}
